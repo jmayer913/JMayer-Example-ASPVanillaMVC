8dc85d6a75efa2652a854dd6ea3a8957f23c0e3c
jmayer913 <72579603+jmayer913@users.noreply.github.com>
2025-12-29 15:07:07 -0500

    Added Initial Project

---
+++ JMayer.Example.ASPVanillaMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JMayer.Example.ASPVanillaMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
