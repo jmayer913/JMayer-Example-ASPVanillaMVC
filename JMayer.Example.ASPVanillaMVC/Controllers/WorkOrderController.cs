982b0683bcc417f78c5c0a5c7c3c93655a2a10d6
jmayer913 <72579603+jmayer913@users.noreply.github.com>
2026-01-04 12:44:27 -0500

    Added readonly work orders page

---
+++ JMayer.Example.ASPVanillaMVC/Controllers/WorkOrderController.cs
@@ -0,0 +1,14 @@
+﻿using JMayer.Example.ASPVanillaMVC.DataLayers;
+using JMayer.Example.ASPVanillaMVC.Models;
+using JMayer.Web.Mvc.Controller.Mvc;
+
+namespace JMayer.Example.ASPVanillaMVC.Controllers;
+
+/// <summary>
+/// The class manages HTTP requests for views and actions associated with work orders.
+/// </summary>
+public class WorkOrderController : StandardModelViewController<WorkOrder, IWorkOrderDataLayer>
+{
+    /// <inheritdoc/>
+    public WorkOrderController(IWorkOrderDataLayer dataLayer, ILogger<WorkOrderController> logger) : base(dataLayer, logger) { }
+}
