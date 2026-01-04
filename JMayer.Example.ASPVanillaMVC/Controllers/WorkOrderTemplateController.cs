3b7d6969581c40c202f08f88fcd3f10dd861ec8f
jmayer913 <72579603+jmayer913@users.noreply.github.com>
2026-01-04 13:39:40 -0500

    Added create work order button

--- JMayer.Example.ASPVanillaMVC/Controllers/WorkOrderTemplateController.cs
+++ JMayer.Example.ASPVanillaMVC/Controllers/WorkOrderTemplateController.cs
@@ -1,7 +1,7 @@
-﻿using JMayer.Data.Database.DataLayer;
-using JMayer.Example.ASPVanillaMVC.DataLayers;
+﻿using JMayer.Example.ASPVanillaMVC.DataLayers;
 using JMayer.Example.ASPVanillaMVC.Models;
 using JMayer.Web.Mvc.Controller.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace JMayer.Example.ASPVanillaMVC.Controllers;
 
@@ -10,6 +10,81 @@ namespace JMayer.Example.ASPVanillaMVC.Controllers;
 /// </summary>
 public class WorkOrderTemplateController : StandardModelViewController<WorkOrderTemplate, IWorkOrderTemplateDataLayer>
 {
+    /// <summary>
+    /// Used to interact with the work order collection.
+    /// </summary>
+    private readonly IWorkOrderDataLayer _workOrderDataLayer;
+
     /// <inheritdoc/>
-    public WorkOrderTemplateController(IWorkOrderTemplateDataLayer dataLayer, ILogger<WorkOrderTemplateController> logger) : base(dataLayer, logger) { }
+    public WorkOrderTemplateController(IWorkOrderTemplateDataLayer dataLayer, ILogger<WorkOrderTemplateController> logger, IWorkOrderDataLayer workOrderDataLayer) : base(dataLayer, logger) 
+        => _workOrderDataLayer = workOrderDataLayer;
+
+    /// <summary>
+    /// The method returns the confirm create work order view.
+    /// </summary>
+    /// <param name="id">The id for the record.</param>
+    /// <returns>The confirm create work order view or a negative status code.</returns>
+    public async Task<IActionResult> ConfirmCreateWorkOrderViewAsync(long id)
+    {
+        try
+        {
+            Logger.LogInformation("Attempting to retrieve the {Type} data object for {ID} for the Confirm Create Work Order View.", DataObjectTypeName, id);
+
+            WorkOrderTemplate? workOrderTemplate = await DataLayer.GetSingleAsync(obj => obj.Integer64ID == id);
+
+            if (workOrderTemplate is null)
+            {
+                Logger.LogError("The {Type} data object for {ID} was not found so the Confirm Create Work Order View could not be returned.", DataObjectTypeName, id);
+                return NotFound();
+            }
+
+            Logger.LogInformation("The {Type} data object for {ID} for the Confirm Create Work Order View was successfully retrieved; returning the view.", DataObjectTypeName, id);
+            return View($"WorkOrderTemplateConfirmCreateWorkOrder", workOrderTemplate);
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "Failed to return the Confirm Create Work Order View for the {Type} data object for {ID}.", DataObjectTypeName, id);
+            return Problem();
+        }
+    }
+
+    /// <summary>
+    /// The method creates a work order from the template.
+    /// </summary>
+    /// <param name="id">The id for the record.</param>
+    /// <returns>The work order index view or a negative status code.</returns>
+    public async Task<IActionResult> CreateWorkOrderAsync(long id)
+    {
+        try
+        {
+            Logger.LogInformation("Attempting to create a work order from the {ID} template.", id);
+
+            WorkOrderTemplate? workOrderTemplate = await DataLayer.GetSingleAsync(obj => obj.Integer64ID == id);
+
+            if (workOrderTemplate is null)
+            {
+                Logger.LogError("The {Type} data object for {ID} was not found so the work order could not be created.", DataObjectTypeName, id);
+                return NotFound();
+            }
+
+            _ = _workOrderDataLayer.CreateAsync(new WorkOrder()
+            {
+                Description = workOrderTemplate.Description,
+                DueBy = workOrderTemplate.DaysDueFromCreation > 0 ? DateTime.Today.AddDays(workOrderTemplate.DaysDueFromCreation) : null,
+                Name = $"{workOrderTemplate.Name} {DateTime.Today.ToShortDateString()}",
+                OtherTypeOfService = workOrderTemplate.OtherTypeOfService,
+                Priority = workOrderTemplate.Priority,
+                ServiceType = workOrderTemplate.ServiceType,
+            });
+
+            Logger.LogInformation("The work order for the {ID} template was successfully created; returning the work order index view.", id);
+
+            return RedirectToAction(nameof(Index), nameof(WorkOrder));
+        }
+        catch (Exception ex)
+        {
+            Logger.LogError(ex, "Failed to create the work order from the {ID} template.", id);
+            return Problem();
+        }
+    }
 }
--- JMayer.Example.ASPVanillaMVC/Program.cs
+++ JMayer.Example.ASPVanillaMVC/Program.cs
@@ -6,9 +6,8 @@ using JMayer.Example.ASPVanillaMVC.Workers;
 //TO DO:
 //I need to make sure start date is less than or equal to end date. Built-in Compare attribute only does equal comparison so I need to make something custom. (Will need to add unit tests for this.)
 //Unit Tests for the worker. (Not 100% sure how to test this since its dependent on the day its ran and right now, I can't simulate the day its ran.)
-//A create work order now button on the schedule page.
-//A view only page for created work orders.
-//Unit Tests for the view only page.
+//Unit Tests for create work order button.
+//See if I can disable the create work order button when the schedule is disabled.
 //Add description to Welcome page.
 //Create a readme.
 
