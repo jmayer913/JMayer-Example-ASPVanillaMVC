720f68468815f1215c8bf0781caf8ec545ce9414
jmayer913 <72579603+jmayer913@users.noreply.github.com>
2026-01-11 10:53:52 -0500

    Minor word changes to home page

--- JMayer.Example.ASPVanillaMVC/Program.cs
+++ JMayer.Example.ASPVanillaMVC/Program.cs
@@ -5,9 +5,8 @@ using JMayer.Example.ASPVanillaMVC.Workers;
 
 //TO DO:
 //I need to make sure start date is less than or equal to end date. Built-in Compare attribute only does equal comparison so I need to make something custom. (Will need to add unit tests for this.)
+//I need to make sure Other Type of Service is required when the Service Type is Other. (I don't think there's a built-in attribute for a conditional require.)
 //Unit Tests for the worker. (Not 100% sure how to test this since its dependent on the day its ran and right now, I can't simulate the day its ran.)
-//Add description to Welcome page.
-//Create a readme.
 
 var builder = WebApplication.CreateBuilder(args);
 
